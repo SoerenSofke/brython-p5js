1c3a2604909f49e91fa8a487125f8da70b0098d9
SoerenSofke <soeren.sofke@gmail.com>
2021-09-04 20:17:43 +0000

    Use p5 class

---
+++ p5.py
@@ -0,0 +1,31 @@
+from browser import window as p, load
+
+
+class P5:
+    def __init__(self):
+        load("https://cdn.jsdelivr.net/npm/p5@1.4/lib/p5.min.js")
+
+    def createCanvas(self, *arg):
+        p.createCanvas(*arg)
+
+    def background(self, *arg):
+        p.background(*arg)
+
+    def fill(self, *arg):
+        p.fill(*arg)
+
+    def circle(self, *arg):
+        p.circle(*arg)
+
+    @property
+    def mouseX(self):
+        return p.mouseX
+
+    @property
+    def mouseY(self):
+        return p.mouseY
+
+
+def export(f):
+    p[f.__name__] = f
+    return f
--- script.py
+++ script.py
@@ -1,25 +1,19 @@
-from browser import window as p, load
-
-load("https://cdn.jsdelivr.net/npm/p5@1.4/lib/p5.min.js")
-
-
-def export(f):
-    p[f.__name__] = f
-    return f
+from p5 import P5, export
+p5 = P5()
 
 
 @export
 def setup():
-    p.createCanvas(400, 400)
-    p.background(0)
+    p5.createCanvas(300, 500)
+    p5.background(0)
 
 
 @export
 def draw():
-    p.fill(255, 255, 0, 128)
-    p.ellipse(p.mouseX, p.mouseY, 50, 50)
+    p5.fill(255, 255, 0, 128)
+    p5.circle(p5.mouseX, p5.mouseY, 20)
 
 
 @export
 def mousePressed(event):
-    p.background(0)
+    p5.background(0)
